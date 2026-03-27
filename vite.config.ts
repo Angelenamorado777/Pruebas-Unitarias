2187d9fd5cb8459cc92789b34cb480c9cd28e395
Angelenamorado777 <albertoenamorado123@gmail.com>
2026-03-26 18:33:40 -0600

    ConfigTests

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+    tailwindcss(),
   ],
+
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './setupTest.ts',
+    reporters: 'verbose',
+
+  }
 })
